e6a7354b303055091547cb7cc95e32f5c39f3e96
previzdaniel <dani.2000.2x@gmail.com>
2020-10-21 14:52:07 +0200

    Add project files.

---
+++ Telekocsi/Igeny.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekocsi
+{
+    class Igeny
+    {
+        public string Azonosito { get; private set; }
+        public string Indulas { get; private set; }
+        public string Cel { get; private set; }
+        public int Szemelyek { get; private set; }
+
+        public Igeny(string szoveg)
+        {
+            string[] adat = szoveg.Split(';');
+            Azonosito = adat[0];
+            Indulas = adat[1];
+            Cel = adat[2];
+            Szemelyek = int.Parse(adat[3]);
+        }
+    }
+}
---
+++ Telekocsi/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Telekocsi
+{
+    class Program
+    {
+        static List<Auto> autok = new List<Auto>();
+        static List<Igeny> igenyek = new List<Igeny>();
+
+        
+
+
+        static void BeolvasAutok()
+        {
+            StreamReader file = new StreamReader("autok.csv");
+            file.ReadLine();
+            while (!file.EndOfStream)
+            {
+                autok.Add(new Auto(file.ReadLine()));
+            }
+            file.Close();
+        }
+
+        static void BeolvasIgenyek()
+        {
+            StreamReader file = new StreamReader("igenyek.csv");
+            file.ReadLine();
+            while (!file.EndOfStream)
+            {
+                igenyek.Add(new Igeny(file.ReadLine()));
+            }
+            file.Close();
+        }
+
+        static void Feladat2()
+        {
+            Console.WriteLine("2. feladat");
+            Console.WriteLine($"   {autok.Count} autos hirdet fuvart.");
+        }
+
+        static void Feladat3()
+        {
+            Console.WriteLine("3. feladat");
+            int ferohely = 0;
+            for (int i = 0; i < autok.Count; i++)
+            {
+                if (autok[i].Indulas == "Budapest" && autok[i].Cel == "Miskolc")
+                {
+                    ferohely += autok[i].Ferohely;
+                }
+            }
+            Console.WriteLine($"   Összesen {ferohely} férőhelyet hirdettek az autósok Budapestről Miskolcra.");
+        }
+
+        static void Feladat4()
+        {
+            #region Dictionary, foreach
+            //Dictionary<string, int> utvonalak = new Dictionary<string, int>();
+
+            //foreach (var a in autok)
+            //{
+            //    if (!utvonalak.ContainsKey(a.Utvonal))
+            //    {
+            //        utvonalak.Add(a.Utvonal, a.Ferohely);
+            //    }
+            //    else
+            //    {
+            //        utvonalak[a.Utvonal] = utvonalak[a.Utvonal] + a.Ferohely;
+            //    }
+            //}
+
+            int max = 0;
+            string utv = "";
+            //foreach (var u in utvonalak)
+            //{
+            //    if (u.Value > max)
+            //    {
+            //        max = u.Value;
+            //        utv = u.Key;
+            //    }
+            //} 
+            #endregion
+
+            var utvonalak = from a in autok
+                            orderby a.Utvonal
+                            group a by a.Utvonal into temp
+                            select temp;
+
+            foreach (var ut in utvonalak)
+            {
+                int fh = ut.Sum(x => x.Ferohely);
+                if (max < fh)
+                {
+                    max = fh;
+                    utv = ut.Key;
+                }
+                //Console.WriteLine($"{ut.Key} - {ut.Count()}");
+            }
+
+            Console.WriteLine("4. feladat");
+            Console.WriteLine($"{max} - {utv}");
+        }
+
+        static void Feladat5()
+        {
+            Console.WriteLine("5. feladat");
+            foreach (var igeny in igenyek)
+            {
+                int i = 0;
+                while (i < autok.Count &&
+                    !(igeny.Indulas == autok[i].Indulas &&
+                    igeny.Cel == autok[i].Cel &&
+                    igeny.Szemelyek <= autok[i].Ferohely))
+                {
+                    i++;
+                }
+
+                if (i < autok.Count)
+                {
+                    Console.WriteLine($"{igeny.Azonosito} => {autok[i].Rendszam}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            BeolvasAutok();
+            BeolvasIgenyek();
+
+            Feladat2();
+            Feladat3();
+            Feladat4();
+            Feladat5();
+
+            Console.ReadKey();
+        }
+    }
+}
